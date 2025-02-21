76eafe5d79999df12435a7e3ec37655297745e00
zeynab jalalian <zjalalianweb@gmail.com>
2025-02-21 19:11:48 +0330

    dom

---
+++ script.js
@@ -0,0 +1,7 @@
+const indicator=document.querySelector('.indicator');
+const weak=document.querySelector('.weak');
+const medium=document.querySelector('.medium');
+const strong=document.querySelector('.strong');
+const text=document.querySelector('.text');
+const show=document.querySelector('.show');
+const input=document.querySelector('input');
